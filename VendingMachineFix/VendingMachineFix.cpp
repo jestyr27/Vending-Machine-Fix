ff725e6805b4195851ca0b6c4d4202249571a7c2
alandati <huntinater6k@ymail.com>
2017-04-18 16:35:28 -0400

    Update VendingMachineFix.cpp

--- VendingMachineFix/VendingMachineFix.cpp
+++ VendingMachineFix/VendingMachineFix.cpp
@@ -6,17 +6,51 @@
 #include <iomanip>
 #include "VendingSlot.h"
 #include <string>
+#include <Windows.h>
+#include <stdlib.h>
+#include <vector>
+#include <conio.h>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+//NEW
 using namespace std;
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 //constants
 const int MAX_COLUMNS = 8;
 const int MAX_ROWS = 5;
 const int MAX_SLOTS = MAX_ROWS * MAX_COLUMNS;
 
-
+//chars
+char r = int(205);
+char w = int(186);
+char s = ' ';
 //strings
 string input;
-
+//NEW
+char displayMachine[1197] = {r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,s,s,s,s,s,s,w,
+						r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,};
 //ints
 int slotToGet;
 
@@ -27,44 +61,68 @@ VendingSlot slot[MAX_SLOTS];
 bool buying = true;
 
 //prototypes
-int calcIndex();
+int calcIndex(int input);
 void getInput();
-
+void displayMachines();//NEW
+void setCursor(int coordX, int coordY);
+void initializeMachine();
+void displaySlots();
 //food symbols
-char one = int(185);
 
 
+//misc
+COORD coord;
 
-int calcIndex() {
+
+int calcIndex(){
+	//represents row number
 	int left = input[0];
+	//represents column number
 	int right = input[1];
+
+	//adjusts for ASCII problems
 	left -= 48;
 	right -= 48;
 
-	return(((left - 1) * MAX_COLUMNS + right - 1));
+	//returns a value that is the user input in format of row + column into a contiguous slot number
+	return(   ((left-1) * MAX_COLUMNS + right - 1)   );
 
 
 }
 
-void getInput() {
+void setCursor(int coordX, int coordY) {
+	coord.X = coordX;
+	coord.Y = coordY;
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void getInput(){
 	int left;
 	int right;
 	bool good = false;
+	
 
-
-	while (good == false) {
-		cout << "\nWhich item would you like?";
+	while (good == false){
+		setCursor(0,21);
+		cout << "\nWhich item would you like?   ";
 		cin >> input;
-
-		if (input.length() == 2) {
+		//checks for user input being correct length
+		if(input.length() == 2){
+			//converts input into rows and columns
 			left = input[0];
 			right = input[1];
 			left -= 48;
 			right -= 48;
-
-			if (left > 0 && left <= MAX_ROWS) {
-
-				if (right > 0 && right <= MAX_COLUMNS) {
+			
+			int realSlot = calcIndex();
+			//determines if the rows/columns are valid
+			if(left > 0 && left <= MAX_ROWS){
+				
+				if(right > 0 && right <= MAX_COLUMNS){
+					
+					if(slot[realSlot].getSlotQuantity() == 0)
+						cout << "Product not available";
 
 					good = true;
 				}
@@ -72,39 +130,74 @@ void getInput() {
 		}
 	}
 
+	
 
+}
 
+void displayMachines(){
+	for(int i = 0; i < sizeof(displayMachine); i++){
+		
+		if(i%57==0)
+			cout << "\n";
+		cout << displayMachine[i];
+	}
+}
+
+void initializeMachine(){
+	
+	int slotCounter = 0;
+	for(int i = 0; i < MAX_ROWS; i++)
+	{
+		
+		for(int j = 0; j < MAX_COLUMNS; j++){
+			
+			slot[slotCounter].setSlotNumber((i*10) + j);
+			slotCounter++;
+		}
+	}
+}
+
+void displaySlots(){
+	
+	
+	int slotCounter = 0;
+	for(int i = 0; i < MAX_ROWS; i++)
+	{
+		for(int j = 0; j < MAX_COLUMNS; j++){
+			//i = y = rows, j = x = columns
+			setCursor( (j*7+3),(i*4+2) );
+			cout <<  i+1 << j+1;
+			slotCounter++;
+		}
+	}
+	
 }
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//sets each slow to default settings
+	initializeMachine();
+	
+	//Show the outline of the machine
+	displayMachines();
+	
 
+	displaySlots();
+	
 
+	slot[10].setSlotPrice(1.25);
+	slot[10].setSlotNumber(21);
+	
 
-
-
-
-	//Set each slot number
-	int slotCounter = 0;
-	for (int i = 0; i < MAX_ROWS; i++)
-	{
-		for (int j = 0; i < MAX_COLUMNS; i++) {
-
-			slot[slotCounter].setSlotNumber((i * 10) + j);
-			slotCounter++;
-		}
-	}
-
-
-
+	
 
 	//Fill each slot with an item
 
 
 
 
-	while (buying == true) {
+	while (buying == true){
 		getInput();
 		slotToGet = calcIndex();
 		cout << "\nInput: " << input << "\nSlot Number: " << slotToGet;
@@ -113,3 +206,4 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	return 0;
 }
+
